8ce876e10f2e304fae957760488ccfb61130325b
Paulo Correia <paulocorreia@wh1.com.br>
2026-09-04 10:15:12 -0300

    Padroniza valores com duas casas decimais

--- js/main.js
+++ js/main.js
@@ -14,7 +14,8 @@ let activePropertyMediaIndex = 0;
 const currency = new Intl.NumberFormat("pt-BR", {
   style: "currency",
   currency: "BRL",
-  maximumFractionDigits: 0,
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
 });
 
 function getWhatsappAudioContext() {
